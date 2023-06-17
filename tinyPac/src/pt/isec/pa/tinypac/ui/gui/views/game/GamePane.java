94c9c9cbccb2bae2aaadcae46dbeb663394069d5
Nogueira <pmsn.mig2002@gmail.com>
2023-06-17 17:43:04 +0100

    adjust gui

--- tinyPac/src/pt/isec/pa/tinypac/ui/gui/views/game/GamePane.java
+++ tinyPac/src/pt/isec/pa/tinypac/ui/gui/views/game/GamePane.java
@@ -2,6 +2,7 @@ package pt.isec.pa.tinypac.ui.gui.views.game;
 
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Label;
 import javafx.scene.control.ToggleButton;
 import javafx.scene.control.ToggleGroup;
 import javafx.scene.input.KeyCode;
@@ -18,14 +19,7 @@ public class GamePane extends BorderPane {
 
     private ModelManager manager;
     private VBox menu;
-    private ToggleGroup tgMenuButtons;
-    private ToggleButton btnChangeDirection;
     private MazePane mazePane;
-    private static final Integer MENU_MIN_WIDTH = 150;
-    private static final Integer BTN_PREF_WIDTH = 150;
-    private static final Integer BTN_MAX_WIDTH = 200;
-    private static final Integer BTN_PREF_HEIGHT = 100;
-    private static final Integer BTN_SPACING = 5;
     public GamePane(ModelManager manager) {
 
         this.manager = manager;
@@ -38,21 +32,12 @@ public class GamePane extends BorderPane {
     private void createViews() {
 
         this.setBackground(new Background(new BackgroundFill(Color.BLACK, CornerRadii.EMPTY, Insets.EMPTY)));
-        tgMenuButtons = new ToggleGroup();
 
+        Label label = new Label("Esc to pause the game");
+        label.getStyleClass().add("Top5BottomLb");
         this.mazePane = new MazePane(manager);
 
-        btnChangeDirection = new ToggleButton("Change Pacman Direction");
-        btnChangeDirection.setToggleGroup(tgMenuButtons);
-        btnChangeDirection.setPrefHeight(BTN_PREF_HEIGHT);
-        btnChangeDirection.setPrefWidth(BTN_PREF_WIDTH);
-        btnChangeDirection.setMaxWidth(BTN_MAX_WIDTH);
-        btnChangeDirection.setSelected(false);
-
-        menu = new VBox(mazePane/*,btnChangeDirection*/);
-       // menu = new VBox(btnChangeDirection);
-        //menu.setMinWidth(1000);
-        //menu.setMinHeight(Integer.MAX_VALUE);
+        menu = new VBox(mazePane, label);
         this.setCenter(menu);
         menu.setAlignment(Pos.CENTER);
     }
